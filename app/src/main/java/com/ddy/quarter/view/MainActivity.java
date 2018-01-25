49698fa6785998ebfb04784a0adaab6afa1eaa8c
2327100351 <2327100351@qq.com>
2018-01-25 16:03:18 +0800

    分包

--- app/src/main/java/com/ddy/quarter/view/MainActivity.java
+++ app/src/main/java/com/ddy/quarter/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ddy.quarter;
+package com.ddy.quarter.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ddy.quarter.R;
 
 public class MainActivity extends AppCompatActivity {
 
